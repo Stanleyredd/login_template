621ea9a333fe50d30100e9a7eb95b761ae654c84
Your Name <stanley2001@live.nl>
2020-03-27 14:32:31 +0100

    small changes

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanley
+ * Date: 27-3-2020
+ * Time: 12:58
+ */
+
+echo "<a href='login.php'>login</a><br>";
+echo "<a href='login.php'>register</a>";
